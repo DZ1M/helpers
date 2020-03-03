d8798ae7abec1e4ecc36b18264f252e55415d6fc
Richard <Richard@DESKTOP-9LM3OJE>
2020-03-03 00:25:19 -0300

    Criando nova classe para as validações. Movendo validações de email e cpf/cnpj. Adicionando validação de Telefone.

--- Demos/StringHelper.cs
+++ Demos/StringHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
-using System.Text.RegularExpressions;
 
 namespace Demos
 {
@@ -126,56 +125,6 @@ namespace Demos
         public static bool VerificaCpfCnpj(this string valor)
         {
             return (VerificaCpf(valor) || VerificaCnpj(valor));
-        }
-
-        public static bool ValidaCpfCnpj(this string text)
-        {
-            text = text.Replace(".", "").Replace("-", "").Replace("/", "").Replace("_", "");
-            if (string.IsNullOrEmpty(text) || Convert.ToInt64(text) == 0)
-            {
-                return false;
-            }
-            else
-            {
-                int[] d = new int[14];
-                int[] v = new int[2];
-                int j, i, soma;
-                string Sequencia, SoNumero;
-                SoNumero = Regex.Replace(text, "[^0-9]", string.Empty);
-                if (new string(SoNumero[0], SoNumero.Length) == SoNumero) return false;
-                if (SoNumero.Length == 11)
-                {
-                    for (i = 0; i <= 10; i++) d[i] = Convert.ToInt32(SoNumero.Substring(i, 1));
-                    for (i = 0; i <= 1; i++)
-                    {
-                        soma = 0;
-                        for (j = 0; j <= 8 + i; j++) soma += d[j] * (10 + i - j);
-
-                        v[i] = (soma * 10) % 11;
-                        if (v[i] == 10) v[i] = 0;
-                    }
-                    return (v[0] == d[9] & v[1] == d[10]);
-                }
-                else if (SoNumero.Length == 14)
-                {
-                    Sequencia = "6543298765432";
-                    for (i = 0; i <= 13; i++) d[i] = Convert.ToInt32(SoNumero.Substring(i, 1));
-                    for (i = 0; i <= 1; i++)
-                    {
-                        soma = 0;
-                        for (j = 0; j <= 11 + i; j++)
-                            soma += d[j] * Convert.ToInt32(Sequencia.Substring(j + 1 - i, 1));
-
-                        v[i] = (soma * 10) % 11;
-                        if (v[i] == 10) v[i] = 0;
-                    }
-                    return (v[0] == d[12] & v[1] == d[13]);
-                }
-                else
-                {
-                    return false;
-                }
-            }
         }
 
         public static bool ValidaEmail(this string email)
---
+++ Demos/ValidacaoHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Demos
+{
+    public static class ValidacaoHelper
+    {
+        public static bool EmailValido(this string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool TelefoneValido(this string telefone)
+        {
+            try
+            {
+                return Regex.Match(telefone, @"(\+[0-9]{2}|\+[0-9]{2}\(0\)|\(\+[0-9]{2}\)\(0\)|00[0-9]{2}|0)([0-9]{9}|[0-9\-\s]{9,18})").Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool ValidaCpfCnpj(this string text)
+        {
+            text = text.Replace(".", "").Replace("-", "").Replace("/", "").Replace("_", "");
+            if (string.IsNullOrEmpty(text) || Convert.ToInt64(text) == 0)
+            {
+                return false;
+            }
+            else
+            {
+                int[] d = new int[14];
+                int[] v = new int[2];
+                int j, i, soma;
+                string Sequencia, SoNumero;
+                SoNumero = Regex.Replace(text, "[^0-9]", string.Empty);
+                if (new string(SoNumero[0], SoNumero.Length) == SoNumero) return false;
+                if (SoNumero.Length == 11)
+                {
+                    for (i = 0; i <= 10; i++) d[i] = Convert.ToInt32(SoNumero.Substring(i, 1));
+                    for (i = 0; i <= 1; i++)
+                    {
+                        soma = 0;
+                        for (j = 0; j <= 8 + i; j++) soma += d[j] * (10 + i - j);
+
+                        v[i] = (soma * 10) % 11;
+                        if (v[i] == 10) v[i] = 0;
+                    }
+                    return (v[0] == d[9] & v[1] == d[10]);
+                }
+                else if (SoNumero.Length == 14)
+                {
+                    Sequencia = "6543298765432";
+                    for (i = 0; i <= 13; i++) d[i] = Convert.ToInt32(SoNumero.Substring(i, 1));
+                    for (i = 0; i <= 1; i++)
+                    {
+                        soma = 0;
+                        for (j = 0; j <= 11 + i; j++)
+                            soma += d[j] * Convert.ToInt32(Sequencia.Substring(j + 1 - i, 1));
+
+                        v[i] = (soma * 10) % 11;
+                        if (v[i] == 10) v[i] = 0;
+                    }
+                    return (v[0] == d[12] & v[1] == d[13]);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
